cebb9abb864c5e309e2a7eaf6238aba6e02fcf82
SINSOKE99 <sin990325@gmail.com>
2020-10-14 02:22:12 +0700

    perbaikan file index

--- Index.php
+++ Index.php
@@ -4,7 +4,7 @@ echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 
 echo "<h3> Isi Nama dan Nim Anda di bawah baris code ini </h3>";
 
-echo "<h3> WILDAN IBNU SINA (195410210) </h3>"
+echo "<h3> WILDAN IBNU SINA (1954102100) </h3>"
 
 
 ?>
